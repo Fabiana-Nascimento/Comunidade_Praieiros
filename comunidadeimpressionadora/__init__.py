e4489452836dee17f6cfdb032b97673240397893
Fabiana-Nascimento <164015191+Fabiana-Nascimento@users.noreply.github.com>
2024-03-21 13:48:46 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -1,8 +1,11 @@
+import sqlalchemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import SQLAlchemy
+
 
 app = Flask(__name__)
 
@@ -17,6 +20,17 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        print('Base de dados criada')
+else:
+    print('Base de dados ja existente')
 
 # Agora que o objeto SQLAlchemy está criado, importe os módulos de rotas
 from comunidadeimpressionadora import routes
